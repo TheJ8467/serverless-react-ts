12049b742e44ff5e0fc5961247024d9957b16595
TheJ8467 <121436038+TheJ8467@users.noreply.github.com>
2023-07-21 11:34:42 +0900

    build nav bar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,15 +3,17 @@ import Searchbar from './main-screen/1F-searh/Searchbar';
 import Linkbars from './main-screen/2F-hashbars/Linkbars';
 import MainSquares from './main-screen/3F-squares/MainSquares';
 import EventRactangle from './main-screen/4F-event-rectangle/EventRactangle';
+import Navbar from './main-screen/5F-navbar/Navbar';
 
 const App: FunctionComponent<{}> = () => {
   return (
-    <>
+    <div>
       <Searchbar />
       <Linkbars />
       <MainSquares />
       <EventRactangle />
-    </>
+      <Navbar />
+    </div>
   );
 };
 
---
+++ src/components/main-screen/5F-navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+import { FunctionComponent as FC } from 'react';
+import { SquareProps } from '../../../interfaces/SquareProps';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { BsSuitHeart } from 'react-icons/bs';
+import { FaRegRectangleList } from 'react-icons/fa6';
+import { BiWinkSmile } from 'react-icons/bi';
+import { RiHomeSmileFill } from 'react-icons/ri';
+
+const Navbar: FC<SquareProps> = ({}) => {
+  return (
+    <nav className="w-full border rounded-t-3xl border-b-0 bg-white sticky bottom-0 z-50 relative">
+      <div className="container mx-auto px-6 py-3 flex justify-between items-center">
+        <div className="flex flex-col items-center ">
+          <AiOutlineSearch size={25} />
+          <p className="text-sm">Search</p>
+        </div>
+        <div className="flex flex-col items-center ">
+          <BsSuitHeart size={25} />
+          <p className="text-sm">Like</p>
+        </div>
+        <div className="absolute left-1/2 transform -translate-x-1/2 mb-5">
+          <RiHomeSmileFill size={75} />
+        </div>
+        <div className="invisible">
+          <FaRegRectangleList size={25} />
+        </div>
+        <div className="flex flex-col items-center ">
+          <FaRegRectangleList size={25} />
+          <p className="text-sm">List</p>
+        </div>
+        <div className="flex flex-col items-center ">
+          <BiWinkSmile size={25} />
+          <p className="text-sm">My info</p>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
